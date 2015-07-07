11fef9d5cc315ba3592fdc5307494cae5aae115c
Anton Stepanov <astepanov@intech-global.com>
2015-07-07 15:55:38 +0300

    add hibernate dependencies

--- src/main/java/com/springapp/mvc/MainController.java
+++ src/main/java/com/springapp/mvc/MainController.java
@@ -2,61 +2,46 @@ package com.springapp.mvc;
 
 
 
-import com.springapp.mvc.hibernate.models.Book;
-import com.springapp.mvc.hibernate.models.Content;
-import com.springapp.mvc.hibernate.models.Music;
-import com.springapp.mvc.hibernate.services.MediaService;
+import com.springapp.mvc.model.Person;
+import com.springapp.mvc.service.PersonService;
+import com.springapp.mvc.service.PersonServiceImpl;
 import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+
 @Controller
 public class MainController {
     private static final Logger logger = Logger.getLogger(MainController.class);
 
+    private PersonService personService;
+
+    @Autowired(required=true)
+    @Qualifier(value="personService")
+    public void setPersonService(PersonService ps){
+        this.personService = ps;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value="/")
     public String start(ModelMap model) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
-        MediaService service = (MediaService) context.getBean("storageService");
-        service.save(getBook());
-        service.save(getTrack());
-//        logger.info("Список всех элементов библиотеки мультимедиа:");
-        for (Content content : service.getAll()) {
-            logger.info(content);
-        }
+
+        return "start";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/db")
+    public String test_db(ModelMap model) {
+        Person person = new Person();
+        person.setCountry("Germany");
+        person.setName("Anton");
+        this.personService.addPerson(person);
         return "start";
     }
 
 
-    private static Content getBook() {
-        Book book = new Book();
-        book.setTitle("Над пропастью во ржи");
-        book.getAuthor().setFirstName("Джером");
-        book.getAuthor().setMiddleName("Дэвид");
-        book.getAuthor().setLastName("Сэлинджер");
-        book.setPageCount(500);
-        return book;
-    }
-
-    private static Content getTrack() {
-        Music track = new Music();
-        track.setTitle("Moby - Lift Me Up");
-        track.getAuthor().setFirstName("Ричард");
-        track.getAuthor().setMiddleName("Мэлвилл");
-        track.getAuthor().setLastName("Холл");
-        track.setBitRate(256);
-        return track;
-    }
-
-//    @RequestMapping(method = RequestMethod.GET, value="/")
-//    public String hiber_test(ModelMap model) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
-//        return "";
-//    }
 
 }
